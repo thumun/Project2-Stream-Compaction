1534732dca60c82b09f82f31842a7e2dbf130602
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-09-11 22:51:17 -0400

    naive case done

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -12,39 +12,96 @@ namespace StreamCompaction {
             return timer;
         }
         
-        __global__ void naiveScan(int n, int* odata, const int* idata, int offset) {
+        // computes inclusive sum!
+        __global__ void kernNaiveScan(int n, int* odata, const int* idata, int offset) {
             int index = threadIdx.x + (blockIdx.x * blockDim.x);
 
             // Check if idx is out of bounds. If yes, return.
-            if (index >= N)
+            if (index >= n)
                 return;
 
-            // need to add 0 to beginning somewhere?? 
+            // bit shift equivalent to power of 2
+            int pow = 1 << (offset - 1);
 
-            if (index >= offset) {
-                odata[n] = idata[n - offset] + idata[n];
+            // this ver is not in-place (which reads from both in & out arrs)
+            if (index >= pow) {
+                odata[index] = idata[index - pow] + idata[index];
             }
+            else {
+                // takes care of setting elems that are already added in prev iter
+                odata[index] = idata[index];
+            }
+        }
+
+        // to make sure it's exclusive!!
+        // idata = odata here (using my buffers)
+        __global__ void kernShiftArray(int n, int* odata, int*idata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+
+            // don't want the last value here
+            if (index >= n - 1) {
+                return;
+            }
+
+            odata[index+1] = idata[index];
         }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
             // TODO
             
-            // make 2 buffers a & b
-            // write to one
-            // read from other
+            int blockSize = 128;
+            dim3 fullBlocksPerGrid((n + blockSize - 1)/blockSize);
 
+            // read buffer
+            int* dev_dataA;
+            // write buffer
+            int* dev_dataB; 
 
+            // CUDA memory management and error checking.
+            cudaMalloc((void**)&dev_dataA, n * sizeof(int));
+            checkCUDAError("cudaMalloc dataA failed!");
 
-            // d = 1 to ln(n) 
-            // loop through above && create threads for scan
+            cudaMalloc((void**)&dev_dataB, n * sizeof(int));
+            checkCUDAError("cudaMalloc dataB failed!");
+
+            // copying idata into buffer
+            cudaMemcpy(dev_dataA, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
             
-            // swap buffers
+            timer().startGpuTimer();
+
+            for (int d = 1; d <= ilog2ceil(n); d++) {
+                // typical CUDA kernel invocation.
+                kernNaiveScan <<< fullBlocksPerGrid, blockSize >>> (n, dev_dataB, dev_dataA, d);
+                checkCUDAError("NaiveScan failed!");
+
+                // synchronize
+                cudaDeviceSynchronize();
+
+                // swap buffers
+                int* tempPtr = dev_dataB;
+                dev_dataB = dev_dataA;
+                dev_dataA = tempPtr;
+            }
+            
+            // exclusive process b/c above is inclusive
+            // shift array to right by 1
+            kernShiftArray << < fullBlocksPerGrid, blockSize >> > (n, dev_dataB, dev_dataA);
+            checkCUDAError("ShiftArray failed!");
 
             timer().endGpuTimer();
+
+            // putting data into odata
+            cudaMemcpy(odata, dev_dataB, sizeof(int) * n, cudaMemcpyDeviceToHost);
+
+            // setting the first elem to identity
+            // doing here to prevent branch (does this matter?)
+            odata[0] = 0;
+
+            cudaFree(dev_dataA);
+            cudaFree(dev_dataB);
         }
     }
 }
