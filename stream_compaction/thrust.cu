88bf6dc6774dd44de7170293ec06b2af027d4cf9
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-09-12 23:13:16 -0400

    need to test

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -18,11 +18,19 @@ namespace StreamCompaction {
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            thrust::host_vector<int> h_in(n, &idata);
+            thrust::host_vector<int> h_out(n);
+            thrust::device_vector<int> dev_thrust_input = h_in;
+            thrust::device_vector<int> dev_thrust_output = h_out;
+
             timer().startGpuTimer();
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::exclusive_scan(dev_thrust_input.begin(), dev_thrust_input.end(), dev_thrust_output.begin());
             timer().endGpuTimer();
+
+            thrust::copy(dev_thrust_output.begin(), dev_thrust_output.end(), odata);
         }
     }
 }
